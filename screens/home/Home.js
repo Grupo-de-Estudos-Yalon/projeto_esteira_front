a858e7a2b9fad189ff8890b689fa7e2baf3783dd
João Paulo <jpbsampaio@hotmail.com>
2023-10-17 17:27:06 -0300

    feat: inicializando tela home

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,9 +1,9 @@
 import { View, Text } from 'react-native';
-
+import styles from './Style';
 export function HomeScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
+      <View>
+        <Text style={styles.text}>Hayai</Text>
       </View>
     );
   }
---
+++ screens/home/Style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    text: {
+        textAlign: 'left',
+        color: 'red'
+        
+    },
+});
+
+export default styles;
