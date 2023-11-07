7e7e03fdd4b07c662c46327868dd10a5f4823afb
João Paulo <jpbsampaio@hotmail.com>
2023-11-07 00:48:14 -0300

    Feat: Adiciona rota BackEnd

--- screens/home/Home.js
+++ screens/home/Home.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native';
 import styles from './Style';
 import Button from '../../src/components/Button';
-
 export function HomeScreen() {
-  const singIn = () => {
-    alert('cliquei');
-  }
+
+
+const redirecting = () => {
+  window.open('http://localhost:3000/api/user/login', "_blank", "noreferrer");
+}
+
+
     return (
       <View style={styles.container}>
         <Text style={styles.kanji}>速い</Text>
@@ -14,7 +17,7 @@ export function HomeScreen() {
           precisamos que autorize nosso acesso à
           algumas funcionalidades de sua conta no Spotify
         </Text>
-        <Button labelButton={"Login com Spotify"} onpress={singIn}/>
+        <Button labelButton={"Login com Spotify"} onpress={redirecting}/>
       </View>
     );
   }
